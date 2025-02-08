bf5c2569b75d2c5fee0d0e4a5f941ea53b5f9216
gmendesfonseca <fonseca.gabriel.mendes@gmail.com>
2025-02-08 16:06:57 -0300

    feat: :sparkles: add initial application setup with properties, Maven configuration, and basic tests; remove unused GUI classes

---
+++ src/main/java/watt_vision/WattVisionApplication.java
@@ -0,0 +1,13 @@
+package watt_vision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattVisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattVisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/watt_vision/WattVisionApplicationTests.java
@@ -0,0 +1,13 @@
+package watt_vision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattVisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
